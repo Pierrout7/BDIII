9f52e8ebb45cc96b3188675d927c3ba923ea674e
Pierrou7 <92757546+Pierrout7@users.noreply.github.com>
2022-08-30 20:02:58 -0300

    Update BD3 - 221.sql

--- BD3 - 221.sql
+++ BD3 - 221.sql
@@ -9,3 +9,19 @@ create table vendas(
 
 insert into vendas (produto, quantidade, data_venda) 
 values ('Audi TT', 2, '2022-06-10');
+insert into vendas (produto, quantidade, data_venda)
+values ('BMW', 3, '2022-05-01');
+insert into vendas (produto, quantidade, data_venda)
+values ('Ferrari', 10, '2022-07-12');
+insert into vendas (produto, quantidade, data_venda)
+values ('Masserati', 1, '2022-08-22');
+insert into vendas (produto, quantidade, data_venda) 
+values ('Bentley', 4, '2022-08-30');
+
+-- pegando valores entre um e outro, inclusive
+select * from vendas
+where 
+quantidade between 3 and 9;
+
+select * from vendas
+where data_venda between '2022-06-01' and '2022-08-25';
